55f27352896671bf53a768e050657704e873617b
Clint <clint4life@gmail.com>
2024-04-02 18:22:25 -0400

    first commit

---
+++ app/(pages)/cars.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const cars = () => {
+  return (
+    <View>
+      <Text>Car Selection Page</Text>
+    </View>
+  );
+};
+
+export default cars;
+
+const styles = StyleSheet.create({});
